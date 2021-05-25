cd96798acf5a497d8a6e94273f10dcc3e7fa8756
Biswajit Pradhan <biswajit.pradhan@nettantra.net>
2021-05-25 11:27:55 +0530

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,18 @@
 @include('welcome')
 <div class="col-sm-8 offset-sm-2">
-<div class="card mt-4">
-@if(session()->has('message'))
-            <div class="alert {{session('alert') ?? 'alert-info'}}">
-                {{ session('message') }}
-            </div>
- @endif
+    <div class="card mt-4">
+        @if(session()->has('message'))
+        <div class="alert {{session('alert') ?? 'alert-info'}}">
+            {{ session('message') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
         <h5 class="card-header">Enquiry</h5>
         <div class="card-body">
-            <form  method="post" action="{{ route('enquiry') }}" >
-            @csrf
+            <form method="post" action="{{ route('enquiry') }}">
+                @csrf
                 <div class="form-group">
                     <label for="name">Name</label>
                     <input type="text" class="form-control" id="" name="name" placeholder="Enter Name">
@@ -43,3 +46,6 @@
         </div>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
